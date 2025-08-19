7bdf59912e2b38448dcb2d5c0a5429d9774e7fb8
Kayky garcia <kaka091815@gmail.com>
2025-08-19 19:39:18 -0300

    Criação de mais campos no DataFrame

--- graficos_python.py
+++ graficos_python.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+# data aleatória
 def random_dates(start, end, n=10):
     start_u = start.value//10**9
     end_u = end.value//10**9
@@ -13,15 +14,20 @@ num_datas = 50 # quantidade de datas aleatórias
 
 datas = random_dates(start_date, end_date, n=num_datas)
 
+nomes = ['Manuela', 'Monique', 'Marcela', 'Michele', 'Charlize', 'Paula', 'Paulo', 'Heitor', 'Kayky']
 
+convenios = ['Unimed', 'Amil', 'Bradesco Saúde', 'Sul América', 'NotreDame Intermédica', 'Porto Seguro']
 
+idades = np.random.randint(18,60)
 
 
 df = pd.DataFrame({
-    'data': datas,
-    
-
+    'nome': np.random.choice(nomes, num_datas),
+    'convenio': np.random.choice(convenios, num_datas),
+    'idade': np.random.randint(18, 60, num_datas),
+    'data': datas
 })
 print(df)
+df.to_csv('dados_pacientes.csv', index=False)
 
 
